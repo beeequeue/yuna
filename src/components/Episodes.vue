fc1738a3730d76111a15d20cf102da7962ef369e
Adam <adam.lj.haglund@gmail.com>
2018-11-17 02:24:37 +0100

    simplify call

--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -198,15 +198,10 @@ export default class Episodes extends Vue {
   public async fetchEpisodes() {
     if (this.fetched || !this.idMal) return
 
-    const { idMal, nextAiringEpisode } = this
-
     try {
       this.fetched = true
 
-      this.episodes = await AnimeCache.getSeasonFromMalId({
-        idMal,
-        nextAiringEpisode,
-      })
+      this.episodes = await AnimeCache.getSeasonFromMalId(this)
 
       this.loading = false
 
