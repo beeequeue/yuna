2a2d8cb335711502836d04713f6e2fffdc707314
Adam <adam.lj.haglund@gmail.com>
2018-12-11 20:47:45 +0100

    handle 429s (too many requests) better

--- src/lib/myanimelist.ts
+++ src/lib/myanimelist.ts
@@ -1,4 +1,5 @@
 import request from 'superagent/superagent'
+import { T } from 'rambdax'
 
 import { fetchSeasonFromEpisode } from '@/lib/crunchyroll'
 import { Episode } from '@/types'
@@ -6,24 +7,26 @@ import { RequestResponse, responseIsError } from '@/utils'
 
 const CRUNCHYROLL_PROVIDER_ID = '1'
 
+const handleError = (response: RequestResponse) => {
+  if (response.status === 404) {
+    return []
+  }
+
+  if (response.status === 429) {
+    return Promise.reject('Too many requests, try again later.')
+  }
+
+  return Promise.reject('Something went wrong!')
+}
+
 export const fetchEpisodesOfSeries = async (
   id: string | number,
 ): Promise<Episode[]> => {
   const baseUrl = `https://myanimelist.net/anime/${id}`
-  const episodeResponse = (await request
-    .get(baseUrl)
-    .ok(() => true)) as RequestResponse
+  const episodeResponse = (await request.get(baseUrl).ok(T)) as RequestResponse
 
   if (responseIsError(episodeResponse)) {
-    if (episodeResponse.status === 404) {
-      return []
-    }
-
-    if (episodeResponse.status === 429) {
-      return Promise.reject('Too many requests, try again later.')
-    }
-
-    return Promise.reject('Something went wrong!')
+    return handleError(episodeResponse)
   }
 
   const episodesLinkMatch = episodeResponse.text.match(
@@ -34,7 +37,11 @@ export const fetchEpisodesOfSeries = async (
   }
 
   const videoUrl = episodesLinkMatch[0] + '/1'
-  const response = await request.get(videoUrl)
+  const response = (await request.get(videoUrl).ok(T)) as RequestResponse
+
+  if (responseIsError(response)) {
+    return handleError(response)
+  }
 
   const providerMatch = /"provider_id":(\d)/m.exec(response.text)
   if (!providerMatch || providerMatch[1] !== CRUNCHYROLL_PROVIDER_ID) {
@@ -43,8 +50,8 @@ export const fetchEpisodesOfSeries = async (
 
   const mediaIdMatch = /"provider_episode_id":\s?(\d+)/m.exec(response.text)
   if (!mediaIdMatch || !mediaIdMatch[1]) {
-    // tslint:disable-next-line:no-console quotemark
-    console.error("Couldn't find media_id for " + id)
+    // tslint:disable-next-line:no-console
+    console.error(`Couldn't find media_id for ${id}`)
 
     return []
   }
