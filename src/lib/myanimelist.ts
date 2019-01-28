f8e7682102f1602d24be99610b8ef5575adcf98b
Adam <adam.lj.haglund@gmail.com>
2019-01-28 23:05:24 +0100

    remove unnecessary slash

--- src/lib/myanimelist.ts
+++ src/lib/myanimelist.ts
@@ -7,7 +7,7 @@ import { EpisodeListEpisodes } from '@/graphql/types'
 import { Crunchyroll } from '@/lib/crunchyroll'
 import { RequestResponse, responseIsError } from '@/utils'
 
-const baseUrl = `https://myanimelist.net/anime/`
+const baseUrl = `https://myanimelist.net/anime`
 const CRUNCHYROLL_PROVIDER_ID = '1'
 
 const malLimiter = new Bottleneck({
