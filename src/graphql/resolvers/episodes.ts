ad7394cfa20029b5030a6e58c0a12a9e480d3e1f
Adam <adam.lj.haglund@gmail.com>
2019-04-20 21:42:03 +0200

    extract methods from episode resolver

--- src/graphql/resolvers/episodes.ts
+++ src/graphql/resolvers/episodes.ts
@@ -19,6 +19,94 @@ const episodesExist = (
   episodes: EpisodeListEpisodes[] | null | undefined,
 ): episodes is EpisodeListEpisodes[] => !isNil(episodes) && episodes.length >= 1
 
+const getEpisodesFromCache = (
+  cache: RealProxy,
+  id: number,
+  provider: Provider,
+) => {
+  let episodes: EpisodeListEpisodes[] | null
+
+  episodes = getSoftCachedEpisodes(cache, id, provider)
+
+  if (!episodesExist(episodes)) {
+    const hardCachedEpisodes =
+      oc(EpisodeCache.get(id, provider)).episodes() || null
+
+    if (!episodesExist(hardCachedEpisodes)) {
+      episodes = hardCachedEpisodes
+    }
+  }
+
+  return episodes
+}
+
+const fetchEpisodesFromCrunchyroll = async (
+  cache: RealProxy,
+  id: number,
+  provider: Provider,
+) => {
+  const idMal = getCachedAnimeIdMal(cache, id)
+  let unconfirmedEpisodes
+
+  if (!isNil(idMal)) {
+    try {
+      unconfirmedEpisodes = await fetchEpisodesOfSeries(id, idMal)
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  if (!episodesExist(unconfirmedEpisodes)) {
+    try {
+      unconfirmedEpisodes = await AniDB.getEpisodesForAnime(id)
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  if (unconfirmedEpisodes == null) return null
+
+  const episodesWithCorrectProvider = unconfirmedEpisodes.map(ep => ({
+    ...ep,
+    provider: provider,
+  }))
+
+  const relations = getEpisodeRelations(id, episodesWithCorrectProvider)
+
+  cacheRelations(cache, relations)
+
+  return relations[id]
+}
+
+const fetchEpisodesFromHidive = async (cache: RealProxy, id: number) => {
+  const externalLinks = getCachedExternalLinks(cache, id) || []
+  const hidiveLink = externalLinks.find(propEq('site', 'Hidive'))
+
+  if (isNil(hidiveLink)) {
+    throw new Error('Could not find link to Hidive.')
+  }
+
+  let unconfirmedEpisodes: EpisodeListEpisodes[] | null = null
+  try {
+    unconfirmedEpisodes = (await Hidive.fetchEpisodesByUrl(
+      id,
+      hidiveLink.url,
+    )) as any
+  } catch (err) {
+    throw new Error(err)
+  }
+
+  if (isNil(unconfirmedEpisodes)) {
+    return []
+  }
+
+  const relations = getEpisodeRelations(id, unconfirmedEpisodes)
+
+  cacheRelations(cache, relations)
+
+  return relations[id]
+}
+
 interface EpisodeVariables {
   id: number
   provider: Provider
@@ -36,79 +124,15 @@ export const EpisodesResolver = async (
   let episodes: EpisodeListEpisodes[] | null = null
 
   if (!isStale) {
-    episodes = getSoftCachedEpisodes(cache, id, provider)
-
-    if (!episodesExist(episodes)) {
-      const hardCachedEpisodes =
-        oc(EpisodeCache.get(id, provider)).episodes() || null
-
-      if (!episodesExist(hardCachedEpisodes)) {
-        episodes = hardCachedEpisodes
-      }
-    }
+    episodes = getEpisodesFromCache(cache, id, provider)
   }
 
   // Don't check providers if episodes == [] as that means they don't have it. null means we don't know.
   if (isStale || isNil(episodes)) {
     if ([Provider.Crunchyroll, Provider.CrunchyrollManual].includes(provider)) {
-      const idMal = getCachedAnimeIdMal(cache, id)
-      let unconfirmedEpisodes
-
-      if (!isNil(idMal)) {
-        try {
-          unconfirmedEpisodes = await fetchEpisodesOfSeries(id, idMal)
-        } catch (err) {
-          throw new Error(err)
-        }
-      }
-
-      if (!episodesExist(unconfirmedEpisodes)) {
-        try {
-          unconfirmedEpisodes = await AniDB.getEpisodesForAnime(id)
-        } catch (err) {
-          throw new Error(err)
-        }
-      }
-
-      if (unconfirmedEpisodes == null) return null
-
-      const episodesWithCorrectProvider = unconfirmedEpisodes.map(ep => ({
-        ...ep,
-        provider: provider,
-      }))
-
-      const relations = getEpisodeRelations(id, episodesWithCorrectProvider)
-
-      cacheRelations(cache, relations)
-
-      episodes = relations[id]
+      episodes = await fetchEpisodesFromCrunchyroll(cache, id, provider)
     } else if (provider === Provider.Hidive) {
-      const externalLinks = getCachedExternalLinks(cache, id) || []
-      const hidiveLink = externalLinks.find(propEq('site', 'Hidive'))
-
-      if (isNil(hidiveLink)) {
-        throw new Error('Could not find link to Hidive.')
-      }
-
-      let unconfirmedEpisodes: EpisodeListEpisodes[] | null = null
-      try {
-        unconfirmedEpisodes = (await Hidive.fetchEpisodesByUrl(
-          id,
-          hidiveLink.url,
-        )) as any
-      } catch (err) {
-        throw new Error(err)
-      }
-
-      if (isNil(unconfirmedEpisodes)) {
-        return []
-      }
-
-      const relations = getEpisodeRelations(id, unconfirmedEpisodes)
-
-      cacheRelations(cache, relations)
-
-      episodes = relations[id]
+      episodes = await fetchEpisodesFromHidive(cache, id)
     }
   }
 
