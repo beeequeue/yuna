d3e8ffd5672ece5a2dd94d1f07902fa5dc1a6fe6
Adam <adam.lj.haglund@gmail.com>
2019-05-04 21:37:14 +0200

    add episode finding and thumbnail generating

---
+++ src/utils/paths.ts
@@ -0,0 +1,23 @@
+import { path as ffmpegPath } from 'ffmpeg-static'
+import { path as ffprobePath } from 'ffprobe-static'
+import { join } from 'path'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+const getDevPath = (name: string, path: string) =>
+  join(
+    __dirname,
+    `../../../../../${name}`,
+    path.substr(path.lastIndexOf('bin')),
+  )
+const getProdPath = (name: string, path: string) =>
+  join(
+    process.resourcesPath!,
+    `node_modules/${name}`,
+    path.substr(path.lastIndexOf('bin')),
+  )
+const getPath = (name: string, path: string) =>
+  isDevelopment ? getDevPath(name, path) : getProdPath(name, path)
+
+export const FFMPEG_PATH = getPath('ffmpeg-static', ffmpegPath)
+export const FFPROBE_PATH = getPath('ffprobe-static', ffprobePath)
