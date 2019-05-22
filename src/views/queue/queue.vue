778475cab053be7b84a99af64e41433ced8d93b6
Adam <adam.lj.haglund@gmail.com>
2019-05-23 01:02:15 +0200

    fix queue going outside window

--- src/views/queue/queue.vue
+++ src/views/queue/queue.vue
@@ -378,11 +378,13 @@ export default class Queue extends Vue {
 @import '../../colors';
 
 .queue {
-  position: relative;
   display: flex;
 
-  width: 100%;
-  height: 100%;
+  position: absolute;
+  top: 80px;
+  left: 0;
+  right: 0;
+  bottom: 0;
   background: rgba(0, 0, 0, 0.85);
 
   .queue-container {
