870414784e27d991ca1f91a3020086a76989f8ca
Adam <adam.lj.haglund@gmail.com>
2019-09-17 22:51:13 +0200

    refactor animated-height to support width and better animations

--- src/common/components/animated-height.vue
+++ src/common/components/animated-height.vue
@@ -2,9 +2,11 @@
   <div
     ref="container"
     class="animated-height"
-    :style="{ height: height + 'px' }"
+    :style="{ height: `${height}px`, width: `${width}px` }"
   >
-    <slot />
+    <transition name="h-w" mode="out-in">
+      <slot />
+    </transition>
   </div>
 </template>
 
@@ -19,38 +21,57 @@ export default class AnimatedHeight extends Vue {
   }
 
   public height = 0
+  public width = 0
   public observer!: MutationObserver
 
   public mounted() {
-    this.observer = new MutationObserver(this.updateHeight)
+    this.observer = new MutationObserver(() => this.update())
     this.observer.observe(this.$refs.container, {
       attributes: true,
       subtree: true,
       childList: true,
     })
 
-    this.updateHeight()
+    this.update()
   }
 
-  public updateHeight() {
+  private getExtreme(
+    children: Element[],
+    property: 'top' | 'bottom' | 'left' | 'right',
+  ) {
+    const extreme = ['top', 'left'].includes(property) ? 'min' : 'max'
+
+    return children.reduce(
+      (last, child) => {
+        const value = child.getBoundingClientRect()[property]
+
+        return Math[extreme](value, last)
+      },
+      extreme === 'min' ? Infinity : -Infinity,
+    )
+  }
+
+  public update(type: 'height' = 'height') {
     this.$nextTick(() => {
       if (isNil(this.$refs.container)) {
         return
       }
 
-      const children = this.$refs.container.children
+      const children = Array.from(this.$refs.container.children)
 
       if (children.length < 1) {
-        this.height = 0
+        this[type] = 0
 
         return
       }
 
-      const lastChild = children.item(children.length - 1) as HTMLElement
-      this.height =
-        lastChild.offsetHeight +
-        lastChild.offsetTop +
-        parseInt(getComputedStyle(lastChild).marginBottom || '0')
+      const top = this.getExtreme(children, 'top')
+      const bottom = this.getExtreme(children, 'bottom')
+      const left = this.getExtreme(children, 'left')
+      const right = this.getExtreme(children, 'right')
+
+      this.height = bottom - top
+      this.width = right - left
     })
   }
 }
@@ -59,8 +80,27 @@ export default class AnimatedHeight extends Vue {
 <style scoped lang="scss">
 .animated-height {
   position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   overflow: hidden;
 
-  transition: height 0.25s;
+  transition: height 0.25s, width 0.25s;
+
+  & /deep/ *.h-w {
+    &-enter-active,
+    &-leave-active {
+      transition: opacity 0.25s;
+    }
+
+    &-enter,
+    &-leave-to {
+      opacity: 0;
+    }
+
+    &-enter-active {
+      transition-delay: 0.25s;
+    }
+  }
 }
 </style>
