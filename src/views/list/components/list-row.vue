6b79f4c999dbe73b10e1b6c85b492d1587df1f3e
Adam <adam.lj.haglund@gmail.com>
2019-09-18 23:18:50 +0200

    improve styles slightly

--- src/views/list/components/list-row.vue
+++ src/views/list/components/list-row.vue
@@ -12,28 +12,30 @@
     </div>
 
     <transition>
-      <transition-group
-        v-if="list.length > 0 && open"
-        tag="div"
-        key="EntryContainer"
-        class="entry-container"
-        :class="classes"
-        @wheel.native="handleScroll"
-      >
-        <list-entry
-          v-for="entry in list"
-          :key="entry.id"
-          :entry="entry"
-          :media="media[entry.mediaId]"
-        />
+      <keep-alive>
+        <transition-group
+          v-if="list.length > 0 && open"
+          tag="div"
+          key="EntryContainer"
+          class="entry-container"
+          :class="classes"
+          @wheel.native="handleScroll"
+        >
+          <list-entry
+            v-for="entry in list"
+            :key="entry.id"
+            :entry="entry"
+            :media="media[entry.mediaId]"
+          />
 
-        <div
-          v-if="list.length > 4"
-          key="last"
-          class="padding"
-          v-visibility="fetchMore(status)"
-        />
-      </transition-group>
+          <div
+            v-if="list.length > 4"
+            key="last"
+            class="padding"
+            v-visibility="fetchMore(status)"
+          />
+        </transition-group>
+      </keep-alive>
     </transition>
   </div>
 </template>
@@ -88,9 +90,15 @@ export default class ListRow extends Vue {
 @import '../list-variables';
 
 .list {
+  transition: background 0.15s;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.025);
+  }
+
   & > .title-bar {
     width: 100%;
-    padding: 8px 25px;
+    padding: 5px 25px;
     display: flex;
     align-items: center;
     font-family: 'Raleway', sans-serif;
@@ -99,10 +107,6 @@ export default class ListRow extends Vue {
     font-size: 1.5em;
     cursor: pointer;
     transition: padding-bottom 0.25s;
-
-    &.empty {
-      padding-bottom: 0;
-    }
 
     & > .collapser {
       height: 25px;
