0ae3263ef9d70fa78042327b849d82b9b8649e3e
Adam <adam.lj.haglund@gmail.com>
2020-01-02 10:07:02 +0100

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,21 @@ module.exports = {
     'no-debugger': 'error',
     'prettier/prettier': 'off',
     'no-extra-boolean-cast': 'off',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      { overrides: { constructors: 'no-public' } },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      { assertionStyle: 'as' },
+    ],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
     warnOnUnsupportedTypeScriptVersion: false,
+    project: './tsconfig.json',
   },
 }
