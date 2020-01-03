814404981282974ca9461de2b6a902cc68aa414e
Adam <adam.lj.haglund@gmail.com>
2020-01-03 19:16:42 +0100

    use native-ext-loader instead of node-loader

--- vue.config.js
+++ vue.config.js
@@ -45,11 +45,6 @@ module.exports = {
 
     config.resolve.extensions.add('.node')
 
-    config.resolve.alias.set(
-      '/root/app/node_modules/anitomy-js/build/Release/anitomy-js.node',
-      'node_modules/anitomy-js/build/Release/anitomy-js.node',
-    )
-
     const svgRules = config.module.rule('svg')
     svgRules.uses.clear()
     svgRules.use('raw-loader').loader('raw-loader')
@@ -58,7 +53,11 @@ module.exports = {
       .rule('native')
       .test(/\.node$/)
       .use('node-loader')
-      .loader('node-loader')
+      .loader(
+        process.env.NODE_ENV === 'production'
+          ? 'native-ext-loader'
+          : 'node-loader',
+      )
 
     // Define
     config.plugin('define').tap(([args]) => {
