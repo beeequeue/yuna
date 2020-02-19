189515df331e3a1dc9c1ef13f949222c68fb3584
Adam <adam.lj.haglund@gmail.com>
2020-02-19 20:55:08 +0100

    remove variable

--- src/vue-apollo.ts
+++ src/vue-apollo.ts
@@ -115,7 +115,7 @@ export const createProvider = (store: Store<any>) => {
         fetchPolicy: 'cache-first',
       },
     },
-    async errorHandler({ networkError, message }) {
+    async errorHandler({ networkError }) {
       if (!isNil(networkError) && (networkError as any)?.statusCode === 429) {
         const currentReservoir = (await limiter.currentReservoir()) || 60
         await limiter.incrementReservoir(-currentReservoir)
